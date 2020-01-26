a8941a71f3843ed84377229afa287d88fc0704bf
John Michael Robey <robey14@gmail.com>
2020-01-26 22:16:33 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
-def countdown(secs)
-  until secs == 0
-    puts "#{secs} SECOND(S)!"
+def countdown(number)
+  until number == 0
+    puts "#{number} SECOND(S)!"
     seconds -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 
-def countdown_with_sleep(secs)
+def countdown_with_sleep(number)
   sleep(5)
-  while secs > 0 
-    puts "#{secs} SECOND(S)!"
+  while number > 0 
+    puts "#{number} SECOND(S)!"
     secs -= 1 
   end
   "HAPPY NEW YEAR!"
