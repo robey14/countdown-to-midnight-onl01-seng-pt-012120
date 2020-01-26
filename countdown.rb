baf4a32971855cd9468ff0531faa73286fb5313e
John Michael Robey <robey14@gmail.com>
2020-01-26 22:07:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,17 @@
 def countdown(secs)
-  while secs > 0
+  until secs == 0
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(secs)
+  sleep(5)
+  while secs > 0 
+    puts "#{seconds} SECOND(S)!"
+    secs -= 1 
   end
   "HAPPY NEW YEAR!"
 end
